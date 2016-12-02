60e878b2ce852d08c086bc2113de15bc824ae8ce
Houfeng <admin@xhou.net>
2016-12-02 14:38:37 +0800

    3.0.0-rc10

--- demo/route/app.js
+++ demo/route/app.js
@@ -1,27 +1,27 @@
-(function () {
+//(function () {
 
-  var Component1 = new mokit.Component({
-    template: `<span m:on:click="clickMe">{{name}}</span>`,
-    properties: { name: 'Component1' },
-    clickMe: function () {
-      alert('My name is' + this.name);
-    }
-  });
+var Component1 = new mokit.Component({
+  template: `<span m:on:click="clickMe">{{name}}</span>`,
+  properties: { name: 'Component1' },
+  clickMe: function () {
+    alert('My name is' + this.name);
+  }
+});
 
-  var Component2 = Component1.extend({
-    properties: { name: 'Component2' },
-  });
+var Component2 = Component1.extend({
+  properties: { name: 'Component2' },
+});
 
-  var router = new mokit.Router();
-  router.map({
-    '/': '/test1',
-    '/test1': Component1,
-    '/test2': Component2
-  });
+var router = new mokit.Router();
+router.map({
+  '/': '/test1',
+  '/test1': Component1,
+  '/test2': Component2
+});
 
-  mokit({
-    element: document.getElementById('app'),
-    router: router
-  }).start();
+mokit({
+  element: document.getElementById('app'),
+  router: router
+}).start();
 
-})();
+//})();
